87dbd10721fc35784079741f313c455478851f44
margaritakostadinova8 <margarita_kostadinova8.abv>
2018-10-06 22:07:27 +0300

    HomeworkWeek1

---
+++ AgeAfter10Years/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AgeAfter10Years
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How old are you?");
+            int age = int.Parse(Console.ReadLine());
+            int result = AgeAfter10Years(age);
+            Console.WriteLine("You will be " + result + " years old after 10 years.");
+        }
+
+        static int AgeAfter10Years(int currentAge)
+        {
+            int ageAfter10Years = currentAge + 10;
+
+            // Do calculations here.
+            
+            return ageAfter10Years;
+        }
+    }
+}
---
+++ HiProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HiProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi I am Margarita Kostadinova");
+        }
+    }
+}
